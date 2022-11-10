09169beca7cd8c31e0131b6d7ea928ed31a92d24
JoelKM <joelkasmor16@gmail.com>
2022-11-10 17:25:27 +0100

    build navbar

---
+++ main.js
@@ -0,0 +1,11 @@
+//NAVIGATE ON CLICK
+document.querySelectorAll('nav li').forEach((listitem)=>{
+    listitem.addEventListener('click', ()=>{
+        var topPosition=document.getElementById(listitem.dataset.page).offsetTop-120;
+        window.scrollTo({
+            top: topPosition,
+            left: 0,
+            behavior: 'smooth'
+        })
+    });
+})
